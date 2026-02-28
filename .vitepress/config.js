39b0dc0b9b9e7f848d2de6534bd813161dfd77d9
Zi-chu <3029410588@qq.com>
2026-03-01 00:30:54 +0800

    Add VitePress configuration for site setup

---
+++ .vitepress/config.js
@@ -0,0 +1,12 @@
+export default {
+  title: "专注空间",
+  description: "Energy station",
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'Reading笔记', link: '/reading' },
+      { text: 'Personal作品集', link: '/portfolio' },
+      { text: '话题/疑问', link: '/topic' }
+    ]
+  }
+}
